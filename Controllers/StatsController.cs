cbce0db226690dc8b09c387915efd2283798a82e
pajabyte <pavel.janecka@solarwinds.com>
2020-12-15 14:34:16 +0100

    Initial

---
+++ Controllers/StatsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace todo_dotnet.Controllers
+{
+    [ApiController]
+    [Route("stats")]
+    public class StatsController : ControllerBase
+    {
+        private readonly ILogger<StatsController> _logger;
+
+        public StatsController(ILogger<StatsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { todosCreated = "42", todosCompleted = "2", todosDeleted = "13" });
+        }
+    }
+}
